d9b4f77c5c1294810ecc75975e630d2ba55362a8
Steve <stevehuytrannd92@gmail.com>
2024-10-01 19:28:38 +0700

    update tokenomics

--- src/components/tokenomics/tokenomics1.jsx
+++ src/components/tokenomics/tokenomics1.jsx
@@ -24,20 +24,20 @@ export const TokenNomics = () => {
                     <p className="section-desc">Early birds get the worm! 20% of the nest is set aside for those who swooped in during the presale.</p>
                   </div>
                   <div className="info-card">
-                    <p className="section-heading">Early Bird Catch (20%)</p>
-                    <p className="section-desc">Early birds get the worm! 20% of the nest is set aside for those who swooped in during the presale.</p>
+                    <p className="section-heading">Token Breeding (25%)</p>
+                    <p className="section-desc">A hefty 25% is tucked away for staking. Loyal birds can lay their $FLOCK tokens and watch them hatch into juicy rewards</p>
                   </div>
                   <div className="info-card">
-                    <p className="section-heading">Early Bird Catch (20%)</p>
-                    <p className="section-desc">Early birds get the worm! 20% of the nest is set aside for those who swooped in during the presale.</p>
+                    <p className="section-heading">Flock Vault (25%)</p>
+                    <p className="section-desc">25% is stored in the DAO Vault. This is the $FLOCK treasure chest, used to collectively allocate the Kingdom’s vast riches. The more you vote, the more you earn.</p>
                   </div>
                   <div className="info-card">
-                    <p className="section-heading">Early Bird Catch (20%)</p>
-                    <p className="section-desc">Early birds get the worm! 20% of the nest is set aside for those who swooped in during the presale.</p>
+                    <p className="section-heading">Exchanges Fuel (10%)</p>
+                    <p className="section-desc">We’ve got 10% reserved to keep the skies smooth for our CEX and DEX flights.</p>
                   </div>
                   <div className="info-card">
-                    <p className="section-heading">Early Bird Catch (20%)</p>
-                    <p className="section-desc">Early birds get the worm! 20% of the nest is set aside for those who swooped in during the presale.</p>
+                    <p className="section-heading">Bird Calls (20%)</p>
+                    <p className="section-desc">A full 20% is earmarked for marketing, spreading the word far and wide with squawks, tweets.</p>
                   </div>
               </div>
               <div className="bottom-img">
