fbbab75c63dac8e20819b90234a44d8f413aaffb
Bebune <trungplq@gmail.com>
2024-10-04 02:59:37 +0700

    remove component navigator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Navigation } from "./components/Navigator/navigation";
 import { Header } from "./components/header";
-import { HeaderClaim } from "./components/header_claim";
-import { OutMore } from "./components/ourmore/outmore";
-import { Featured } from "./components/Featured/featured";
 import { About } from "./components/about/about";
-import { MediaContainer } from "./components/media-container/MediaContainer";
-import { Rewards } from "./components/Rewards/rewards";
 import { TokenNomics } from "./components/tokenomics/tokenomics1";
-import { RoadMap } from "./components/Roadmap/roadmap";
-import { Faqs } from "./components/Faqs/faqs";
 import { Footer } from "./components/Footer/footer";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
@@ -32,14 +24,8 @@ const App = () => {
     <div id="container">
       <Navbar />
       <Header/>
-      {/* <OutMore /> */}
-      {/* <MediaContainer />  */}
-      {/* <Featured /> */}
       <About />
-      {/* <Rewards /> */}
       <TokenNomics />
-      {/* <RoadMap />  */}
-      {/* <Faqs /> */}
       <Footer /> 
     </div>
   );
