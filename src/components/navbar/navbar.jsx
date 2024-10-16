3f4872b6f7f82759b88c7950bb961fcc267d95a4
Steve <stevehuytrannd92@gmail.com>
2024-10-16 09:52:58 +0700

    update navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -283,16 +283,16 @@ const Navbar = () => {
             </div>
             <div className={`lang-dropdown-content ${isLanguageOpen ? 'open' : ''}`}>
               {Object.keys(languageOptions).map((language) => (
+                <a href={`/${language}`}>
                 <div key={language} className="lang-dropdown-item" 
                 
                 // onClick={() => handleChangeLanguage(language)}
                 >
-                  <a href={`/${language}`}>
                     <span className={languageOptions[language].flag}></span>
                     <span>{languageOptions[language].name}</span>
-                  </a>
                  
                 </div>
+                </a>
               ))}
             </div>
           </div>
