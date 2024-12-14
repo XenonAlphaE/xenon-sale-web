a90116daf7cd80046e92ffe20f4001b42b483633
Bebune <trungplq@gmail.com>
2024-12-14 23:30:41 +0700

    change price

--- app/client-components/header/header.jsx
+++ app/client-components/header/header.jsx
@@ -300,7 +300,7 @@ export const Header = () => {
                       </div>
                     </div>
 
-                    <p className="total-raised">{sectionText?.funRaised}: $5,012,043.57/ $5,601,303</p>
+                    <p className="total-raised">{sectionText?.funRaised}: $6,510,018.44 / $6,619,225</p>
                       {/* {truncateMiddle(walletEth.currentAddress)} */}
                       <ProgressBar percentage={1481523.67*100 / 1630831}  />
                       <p className="user-purchased-info">{sectionText.boughtAmount} ${configs?.targetToken?.symbol} = {totalBought}</p>
