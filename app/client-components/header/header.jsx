339f50cd202a3b73cd9c5b1241b456d7d3fd6a9c
Bebune <trungplq@gmail.com>
2024-12-15 10:34:33 +0700

    change raise

--- app/client-components/header/header.jsx
+++ app/client-components/header/header.jsx
@@ -305,7 +305,7 @@ export const Header = () => {
                     </div>
                     <ProgressBar percentage={11457475.67*100 / 11540268}  />
 
-                    <p className="total-raised">{sectionText?.funRaised}:  $20,352,005.99 / $20,847,124</p>
+                    <p className="total-raised">{sectionText?.funRaised}:  $21,812,801.93 / $22,011,327</p>
                       {/* {truncateMiddle(walletEth.currentAddress)} */}
                       <p className="user-purchased-info">{sectionText.boughtAmount} ${configs?.targetToken?.symbol} = {totalBought}</p>
                       {/* <img className="img-fluid ms-2 cursor-pointer" src="./img/info-icon.svg" /> */}
