17ee6c01a9aa6ba9e18083e6814e03be90e81e1f
Steve <stevehuytrannd92@gmail.com>
2024-12-20 16:21:07 +0700

    update site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 module.exports = {
-    siteUrl: 'https://wepetoken.com', // Replace with your domain
+    siteUrl: process.env.MAIN_DOMAIN || "https://frenzwith.com", // Replace with your domain
     generateRobotsTxt: true, // Generates robots.txt file along with sitemap
     changefreq: 'daily',
     priority: 0.9,
