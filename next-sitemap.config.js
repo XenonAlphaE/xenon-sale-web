2418337b118d0a49886e281ab49513b9ec4b10d4
Bebune <trungplq@gmail.com>
2024-12-29 18:56:59 +0700

    update url sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 module.exports = {
-    siteUrl: 'https://flockez.com', // Replace with your domain
+    siteUrl: process.env.MAIN_DOMAIN || "https://buldaktoken.com", // Replace with your domain
     generateRobotsTxt: true, // Generates robots.txt file along with sitemap
     changefreq: 'daily',
     priority: 0.7,
