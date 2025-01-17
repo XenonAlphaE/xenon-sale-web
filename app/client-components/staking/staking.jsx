7c29d26caa3ce8f3bf50b8b1059ef21c2f31b147
Steve <stevehuytrannd92@gmail.com>
2025-01-17 23:29:40 +0700

    add stack chart

--- app/client-components/staking/staking.jsx
+++ app/client-components/staking/staking.jsx
@@ -4,6 +4,9 @@
 import React from 'react';
 
 import './staking.css';
+import './staking.mobile.css';
+import { StakingChart } from './stakingchart';
+
 export const Staking = () => {
 
 
@@ -18,7 +21,6 @@ export const Staking = () => {
             <p className='staking-desc'>
                 The distribution of $FLOCK token rewards will occur at a rate of 684.93 $FLOCK tokens per ETH block. These rewards will be disbursed over 2 years and will be claimable once claim goes live.
             </p>
-
             <div className='staking-card-list'>
                 <div className='staking-card'>
                     <div className='staking-card-top'>
@@ -45,95 +47,78 @@ export const Staking = () => {
                 <div className='staking-card'>
                     <div className='staking-card-top'>
                         <div>
-                            Staked Balance
+                            % of Pool
+
+
                         </div>
                         <div>
-                        0 $FLOCK
+                            0%
                         </div>
+                        <div>Total Staked</div>
                         <div>
-                            Your stakeable
-                        </div>
-                        <div>
-                        0 $FLOCK
+                            673,404,199 $FLOCK
 
                         </div>
                     </div>
                     <div className='staking-card-bottom'>
                         <button className='staking-connect-btn'>
-                            BUY AND STAKE
+                            WITHDRAW TOKENS
                         </button>
                     </div>
                 </div>
                 <div className='staking-card'>
                     <div className='staking-card-top'>
                         <div>
-                            Staked Balance
-                        </div>
-                        <div>
-                        0 $FLOCK
-                        </div>
-                        <div>
-                            Your stakeable
-                        </div>
-                        <div>
-                        0 $FLOCK
+                        Estimated Rewards
 
                         </div>
-                    </div>
-                    <div className='staking-card-bottom'>
-                        <button className='staking-connect-btn'>
-                            BUY AND STAKE
-                        </button>
+                        <div>
+                            267% p/a
+                        </div>
+                        <div>
+                        Rewards rate is dynamic
+                        </div>
+                        <div>
+                        Monthly = Rewards % / 12
+                        </div>
+                        <div>
+                        Daily = Rewards % / 365
+                        </div>
                     </div>
                 </div>
                 <div className='staking-card'>
                     <div className='staking-card-top'>
                         <div>
-                            Staked Balance
+                            Current Rewards
                         </div>
                         <div>
-                        0 $FLOCK
+                        684.93 Per ETH Block
                         </div>
-                        <div>
-                            Your stakeable
-                        </div>
-                        <div>
-                        0 $FLOCK
-
-                        </div>
-                    </div>
-                    <div className='staking-card-bottom'>
-                        <button className='staking-connect-btn'>
-                            BUY AND STAKE
-                        </button>
                     </div>
                 </div>
                 <div className='staking-card'>
                     <div className='staking-card-top'>
                         <div>
-                            Staked Balance
+                            Total Rewards
                         </div>
                         <div>
                         0 $FLOCK
                         </div>
-                        <div>
-                            Your stakeable
-                        </div>
-                        <div>
-                        0 $FLOCK
-
-                        </div>
+                      
                     </div>
                     <div className='staking-card-bottom'>
                         <button className='staking-connect-btn'>
-                            BUY AND STAKE
+                            CLAIM REWARDS
                         </button>
                     </div>
                 </div>
             </div>
 
+         
             <div className='staking-chart'>
 
+                <StakingChart />
+                <div className='staking-chart-total'> Total Supply</div>
             </div>
         </div>
     );
--- app/client-components/staking/stakingchart.jsx
+++ app/client-components/staking/stakingchart.jsx
@@ -1,140 +1,139 @@
 
 'use client'; // This component will run on the client side
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { BarChart, Bar, ResponsiveContainer,Cell, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
+import { formatTokenNumber } from '../services/utils';
 import './stakingchart.css';
 export const StakingChart = () => {
+    const [chartMargin, setChartMargin] = useState({ top: 50, right: 50, bottom: 100, left: 50 });
 
+    // Update margins based on screen size
+    const updateMargin = () => {
+        if (window.innerWidth <= 768) {
+        setChartMargin({ top: 20, right: 0, bottom: 100, left: 10 }); // Mobile margins
+        } else {
+        setChartMargin({ top: 50, right: 50, bottom: 100, left: 50 }); // Default margins
+        }
+    };
+
+    useEffect(() => {
+        updateMargin(); // Set the initial margin
+        window.addEventListener('resize', updateMargin); // Listen for window resize
+        return () => window.removeEventListener('resize', updateMargin); // Cleanup on unmount
+    }, []);
+    
+    const data = [
+        {name: 'Sep-2024', value: 9750000000},
+        {name: 'Oct-2024', value: 9900000000},
+        {name: 'Nov-2024', value: 10050000000},
+        {name: 'Dec-2024', value: 10200000000},
+        {name: 'Jan-2025', value: 10350000000},
+        {name: 'Feb-2025', value: 10500000000},
+        {name: 'Mar-2025', value: 10650000000},
+        {name: 'Apr-2025', value: 10800000000},
+        {name: 'May-2025', value: 10950000000},
+        {name: 'Jun-2025', value: 11100000000},
+        {name: 'Jul-2025', value: 11250000000},
+        {name: 'Aug-2025', value: 11400000000},
+        {name: 'Sep-2025', value: 11550000000},
+        {name: 'Oct-2025', value: 11700000000},
+        {name: 'Nov-2025', value: 11850000000},
+        {name: 'Dec-2025', value: 12000000000},
+        {name: 'Jan-2026', value: 12150000000},
+        {name: 'Feb-2026', value: 12300000000},
+        {name: 'Mar-2026', value: 12450000000},
+        {name: 'Apr-2026', value: 12600000000},
+        {name: 'May-2026', value: 12750000000},
+        {name: 'Jun-2026', value: 12900000000},
+        {name: 'Jul-2026', value: 13050000000},
+        {name: 'Aug-2026', value: 13200000000}];
+
+
+    const CustomTooltip = ({ active, payload }) => {
+        if (active && payload && payload.length) {
+            return (
+            <div style={{ backgroundColor: '#555', border: '1px solid #ccc', padding: '10px', borderRadius: '5px', color: 'white' }}>
+                <p><strong>{payload[0].payload.name}</strong></p>
+                <p>{formatTokenNumber(payload[0].value)}</p>
+            </div>
+            );
+        }
+        return null;
+    };
+
+      const formatYAxis = (value) => {
+        if (value >= 1000000000) {
+          return `${(value / 1000000000).toFixed(0)}B`; // Convert to billions with 'B'
+        }
+        return value; // Fallback for smaller values
+      };
+    const getCurrentMonthYearUTC = () => {
+        const now = new Date();
+        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const utcMonth = monthNames[now.getUTCMonth()];
+        const utcYear = now.getUTCFullYear();
+        return `${utcMonth}-${utcYear}`; // Format "MMM-yyyy"
+    };
 
 
     return (
+        <ResponsiveContainer minHeight={500} minWidth={300}>
 
-        <div className='staking-container'>
+            <BarChart 
+               
+                data={data}
+                margin={chartMargin}
 
-            <h1 className='staking-heading'>
-                Welcome to $FLOCK Staking
-            </h1>
-            <p className='staking-desc'>
-                The distribution of $FLOCK token rewards will occur at a rate of 684.93 $FLOCK tokens per ETH block. These rewards will be disbursed over 2 years and will be claimable once claim goes live.
-            </p>
+                >
 
-            <div className='staking-card-list'>
-                <div className='staking-card'>
-                    <div className='staking-card-top'>
-                        <div>
-                            Staked Balance
-                        </div>
-                        <div>
-                        0 $FLOCK
-                        </div>
-                        <div>
-                            Your stakeable
-                        </div>
-                        <div>
-                        0 $FLOCK
+                <CartesianGrid 
+                            stroke="white"               // Set grid line color to white
+                            // strokeDasharray="5 5"        // Create dashed lines with 5px gaps
+                        />
+                <Tooltip content={<CustomTooltip />} />
 
-                        </div>
-                    </div>
-                    <div className='staking-card-bottom'>
-                        <button className='staking-connect-btn'>
-                            BUY AND STAKE
-                        </button>
-                    </div>
-                </div>
-                <div className='staking-card'>
-                    <div className='staking-card-top'>
-                        <div>
-                            Staked Balance
-                        </div>
-                        <div>
-                        0 $FLOCK
-                        </div>
-                        <div>
-                            Your stakeable
-                        </div>
-                        <div>
-                        0 $FLOCK
+                <XAxis 
+                    dataKey="name" 
+                    tick={{ fill: 'black', 
+                        fontSize: 12,
+                        angle: -70, // Rotate the tick labels 50 degrees
+                        textAnchor: 'end', // Align the text for proper rotation
+                    }} 
+                    interval={0}
+                    label={{ 
+                        value: 'Months', 
+                        position: 'bottom', 
+                        offset: 40, 
+                        fill: 'black', 
+                        fontSize: 24 ,
+                    }} 
+                    margin={{ top: 20, right: 20, bottom: 80, left: 80 }} // Adjust the bottom margin
 
-                        </div>
-                    </div>
-                    <div className='staking-card-bottom'>
-                        <button className='staking-connect-btn'>
-                            BUY AND STAKE
-                        </button>
-                    </div>
-                </div>
-                <div className='staking-card'>
-                    <div className='staking-card-top'>
-                        <div>
-                            Staked Balance
-                        </div>
-                        <div>
-                        0 $FLOCK
-                        </div>
-                        <div>
-                            Your stakeable
-                        </div>
-                        <div>
-                        0 $FLOCK
+                    />
+                <YAxis 
+                    tickFormatter={formatYAxis} 
+                    tick={{ fill: 'black', fontSize: 12, fontWeight:900 }} // Text color and font size
+                    label={{ 
+                        value: 'Supply', 
+                        angle: -90, 
+                        position: 'insideLeft', 
+                        fill: 'black', 
+                        fontSize: 24 
+                      }} 
+                />
+                <Bar dataKey="value" radius={[8, 8, 0, 0]}>
+                    {data.map((entry, index) => (
+                    <Cell
+                        key={`cell-${index}`}
+                        fill={ getCurrentMonthYearUTC() === entry.name ? "rgb(245, 136, 45)" : 'rgba(135, 135, 135, 0.5)' } // Example condition for dynamic colors
+                    />
+                    ))}
+                </Bar>
 
-                        </div>
-                    </div>
-                    <div className='staking-card-bottom'>
-                        <button className='staking-connect-btn'>
-                            BUY AND STAKE
-                        </button>
-                    </div>
-                </div>
-                <div className='staking-card'>
-                    <div className='staking-card-top'>
-                        <div>
-                            Staked Balance
-                        </div>
-                        <div>
-                        0 $FLOCK
-                        </div>
-                        <div>
-                            Your stakeable
-                        </div>
-                        <div>
-                        0 $FLOCK
 
-                        </div>
-                    </div>
-                    <div className='staking-card-bottom'>
-                        <button className='staking-connect-btn'>
-                            BUY AND STAKE
-                        </button>
-                    </div>
-                </div>
-                <div className='staking-card'>
-                    <div className='staking-card-top'>
-                        <div>
-                            Staked Balance
-                        </div>
-                        <div>
-                        0 $FLOCK
-                        </div>
-                        <div>
-                            Your stakeable
-                        </div>
-                        <div>
-                        0 $FLOCK
-
-                        </div>
-                    </div>
-                    <div className='staking-card-bottom'>
-                        <button className='staking-connect-btn'>
-                            BUY AND STAKE
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <div className='staking-chart'>
-
-            </div>
-        </div>
+            </BarChart>
+        </ResponsiveContainer>
     );
 };
