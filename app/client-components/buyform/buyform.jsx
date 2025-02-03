4333c615933d5c6b61c0571c14af46cf2804637b
Bebune <trungplq@gmail.com>
2025-02-03 21:37:48 +0700

    change raise

--- app/client-components/buyform/buyform.jsx
+++ app/client-components/buyform/buyform.jsx
@@ -175,7 +175,7 @@ export const BuyForm = () => {
             </div>
             <ProgressBar percentage={11457475.67*100 / 11540268}  />
 
-            <p className="total-raised">{sectionText?.funRaised}:  $66,755,475.98 / $70,000,000</p>
+            <p className="total-raised">{sectionText?.funRaised}:  $68,755,475.98 / $70,000,000</p>
             {walletEth.currentAddress && 
               <div>
                 <p className="user-purchased-info">{sectionText.boughtAmount} ${configs?.targetToken?.symbol} = {walletEth?.formatedBought}</p>
