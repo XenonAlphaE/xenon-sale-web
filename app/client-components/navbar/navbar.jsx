3710a79f4768bfb70c3b1bd49707bcc5451d9e02
Steve <stevehuytrannd92@gmail.com>
2025-02-11 22:43:40 +0700

    add url white paper

--- app/client-components/navbar/navbar.jsx
+++ app/client-components/navbar/navbar.jsx
@@ -122,7 +122,7 @@ export const Navbar = () => {
             {/* <a href="https://x.com/Pepetocoin"  target='_blank' ><img src='/img/pepeto/twitter.svg' className='appnav-social-icon' /></a>
             <a href="https://t.me/pepeto_channel"  target='_blank' ><img src='/img/pepeto/telegram.svg' className='appnav-social-icon' /></a> */}
 
-        <a className="appnav-whitepaper" >{sectionText?.whitePaper}</a>
+        <a href='/img/btcbull/whitepaper.pdf' className="appnav-whitepaper" >{sectionText?.whitePaper}</a>
         <button onClick={scrollToBuySection} className="appnav-login" >{!!currAccount?.address ? truncateMiddle(currAccount?.address) : sectionText?.buyNow}</button>
 
           <div className="appnav-lang-dropdown">
