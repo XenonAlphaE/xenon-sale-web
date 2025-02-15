5da45606d010579e6ded37f338d29a66c09e517d
Steve <stevehuytrannd92@gmail.com>
2025-02-15 09:21:59 +0700

    update raise

--- app/client-components/buyform/buyform.jsx
+++ app/client-components/buyform/buyform.jsx
@@ -170,9 +170,9 @@ export const BuyForm = () => {
                 </div>
 
 
-                <p className="total-raised">{sectionText?.funRaised}:  $$1,637,139.85 / $2,000,000</p>
+                <p className="total-raised">{sectionText?.funRaised}:  $1,637,139.85 / $2,000,000</p>
 
-                <ProgressBar percentage={14820003 *100/ 2000000 }/>
+                <ProgressBar percentage={1637003 *100/ 2000000 }/>
                 {walletEth.currentAddress && 
                 <div>
                 {/* {truncateMiddle(walletEth.currentAddress)} */}
