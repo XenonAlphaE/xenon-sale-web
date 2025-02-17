c6e3fdab299a1ac947324ef494dc1a836efe7bb2
Steve <stevehuytrannd92@gmail.com>
2025-02-17 12:49:33 +0700

    update claim

--- app/client-components/claimform/claimform.jsx
+++ app/client-components/claimform/claimform.jsx
@@ -161,14 +161,14 @@ export const ClaimForm = () => {
         <div className="walletBox" id='walletBox'>
             <div className="walletBox-info">
             <p className="walletBox-heading">THE $WEPE PRESALE IS NOW SOLD OUT</p>
+            
+            <p className="total-raised">Over $70m raised</p>
 
-            {walletEth.currentAddress && 
               <div>
                 <p className="user-purchased-info" style={{fontWeight:800, color:"#1af"}}>Token Address {walletEth?.tokenAddress}</p>
                 <p className="user-purchased-info">{sectionText.boughtAmount} ${configs?.targetToken?.symbol} = {walletEth?.formatedBought}</p>
+                <p className="user-purchased-info">{sectionText.stakeableAmount} ${configs?.targetToken?.symbol} = {walletEth?.formatedBought}</p>
               </div>  
-            }
-            <p className="total-raised">Over $70m raised</p>
             </div>
             {!walletEth.currentAddress && 
             <div className="action-buttons">
