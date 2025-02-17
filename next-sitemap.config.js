3d376a0c75b5ed3b36fcae189d86212ee28e0cb1
Steve <stevehuytrannd92@gmail.com>
2025-02-17 15:12:53 +0700

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,11 +2,29 @@
 require('dotenv').config();
 
 module.exports = {
-    siteUrl: process.env.MAIN_DOMAIN || "https://buldaktoken.com", // Replace with your domain
+    siteUrl: process.env.MAIN_DOMAIN , // Replace with your domain
     generateRobotsTxt: true, // Generates robots.txt file along with sitemap
     changefreq: 'daily',
-    priority: 0.7,
+    priority: 0.9,
     sitemapSize: 7000,
+    additionalPaths: async () => {
+        return [
+            { loc: '/img/btcbull/PRIVACYDOC-BTCBULL.pdf', lastmod: new Date().toISOString() },
+            { loc: '/img/btcbull/COOKIES-BTCBULL.pdf', lastmod: new Date().toISOString() },
+            { loc: '/img/btcbull/Terms-Conditions-BTCBULL.pdf', lastmod: new Date().toISOString() },
+            { loc: '/img/btcbull/audit.pdf', lastmod: new Date().toISOString() },
+            { loc: '/img/btcbull/whitepaper.pdf', lastmod: new Date().toISOString() },
+        ];
+    },
+    robotsTxtOptions: {
+        policies: [
+          {
+            userAgent: '*',
+            allow: ['/', '/img']
+          },
+        ],
+      },
+    
     outDir: './out', // Default output for sitemap generation
 };
   
