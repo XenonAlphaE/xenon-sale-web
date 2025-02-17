c3612a3baf017b2d5e38ba490be9b02535d5bb25
Steve <stevehuytrannd92@gmail.com>
2025-02-17 12:42:26 +0700

    update page name

--- app/client-components/navbar/navbar.jsx
+++ app/client-components/navbar/navbar.jsx
@@ -117,7 +117,7 @@ export const Navbar = ({ isStakingPage = false }) => {
       </div>
 
         <div className={`appnav-navbar-menu`}>
-          <a href="/staking">{sectionText?.staking}</a>
+          <a href="/dashboard">{sectionText?.staking}</a>
           <a href={isStakingPage ? `/${currentLanguage}/#about` : "#about"}>{sectionText?.about}</a>          {/* <a href={`/${currentLanguage}`}>{sectionText?.home}</a> */}
           <a href={isStakingPage ? `/${currentLanguage}/#howtobuy` : "#howtobuy"}>{sectionText?.howtobuy}</a>
           <a href={isStakingPage ? `/${currentLanguage}/#tokenomics` : "#tokenomics"}>{sectionText?.tokenomics}</a>
@@ -158,7 +158,7 @@ export const Navbar = ({ isStakingPage = false }) => {
 
       {isMobile && <div className={`appnav-navbar-menu-mobile ${isMenuOpen ? 'appnav-is-active' : ''}`}>
       {/* <a onClick={toggleMenu} href={`/${currentLanguage}`}>{sectionText?.home}</a> */}
-        <a href="/staking">{sectionText?.staking}</a> 
+        <a href="/dashboard">{sectionText?.staking}</a> 
         <a onClick={toggleMenu} href={isStakingPage ? `/${currentLanguage}/#about` : "#about"} >{sectionText?.about}</a>
         <a onClick={toggleMenu} href={isStakingPage ? `/${currentLanguage}/#howtobuy` : "#howtobuy"} >{sectionText?.howtobuy}</a>
         <a onClick={toggleMenu} href={isStakingPage ? `/${currentLanguage}/#tokenomics` : "#tokenomics"} >{sectionText?.tokenomics}</a>
