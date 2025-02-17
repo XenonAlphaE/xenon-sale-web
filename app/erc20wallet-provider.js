c407b38e1fef2ed53ac73b7c1444b77a37572978
Bebune <trungplq@gmail.com>
2025-02-17 09:32:52 +0700

    change raise

--- app/erc20wallet-provider.js
+++ app/erc20wallet-provider.js
@@ -19,7 +19,7 @@ import { zeroAddress } from "viem";
 // Create a context for the wallet
 const Erc20WalletContext = createContext();
 const lastestUpdated = "2025-02-15T00:00:00Z"
-const lastestRaise  = 1714000.85
+const lastestRaise  = 1933085.72
 const dailyRaise = 40000
 
 
