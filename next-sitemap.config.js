a5c7feb19632d03bcea83f830d8de3ea495c6f73
Steve <stevehuytrannd92@gmail.com>
2025-03-13 13:04:58 +0700

    remove all docs

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,14 +7,6 @@ module.exports = {
     changefreq: 'daily',
     priority: 0.9,
     sitemapSize: 7000,
-    additionalPaths: async () => {
-        return [
-            { loc: '/img/btcbull/PRIVACYDOC-BTCBULL.pdf', lastmod: new Date().toISOString() },
-            { loc: '/img/btcbull/COOKIES-BTCBULL.pdf', lastmod: new Date().toISOString() },
-            { loc: '/img/btcbull/Terms-Conditions-BTCBULL.pdf', lastmod: new Date().toISOString() },
-            { loc: '/img/btcbull/audit.pdf', lastmod: new Date().toISOString() },
-        ];
-    },
     robotsTxtOptions: {
         policies: [
           {
