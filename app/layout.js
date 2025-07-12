c0fc7b29d01f7fc93938a863d0b19f0094feece0
Steve <stevehuytrannd92@gmail.com>
2025-07-12 23:37:07 +0700

    update canonical

--- app/layout.js
+++ app/layout.js
@@ -43,7 +43,7 @@ export async function generateMetadata() {
       image: getOG(), // Path to your Open Graph image,
     },
     alternates: {
-      canonical: "https://btchyper.io", // Set the canonical URL
+      canonical: "", // Set the canonical URL
     },
   };
 }
