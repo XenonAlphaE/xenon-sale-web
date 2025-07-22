5fb2491222b4239d3b9902f9fbefae41555e61e0
Steve <stevehuytrannd92@gmail.com>
2025-07-22 23:56:49 +0700

    update raise

--- app/erc20wallet-provider.js
+++ app/erc20wallet-provider.js
@@ -19,7 +19,7 @@ import { zeroAddress } from "viem";
 // Create a context for the wallet
 const Erc20WalletContext = createContext();
 const lastestUpdated = "2025-07-07T00:00:00Z"
-const lastestRaise  = 266152.22
+const lastestRaise  = 3066152.22
 const dailyRaise = 30000
 const totalRaise = 2400000
 
