d5251521e11031a227eb8bea117ae7a8d3229333
Steve <stevehuytrannd92@gmail.com>
2025-08-22 22:41:01 +0700

    update nav

--- app/client-components/navbar/navbar.jsx
+++ app/client-components/navbar/navbar.jsx
@@ -90,15 +90,17 @@ export const Navbar = ({ isStakingPage = false }) => {
     <div className={styles.navContainer} >
      
         <a className={styles.appnavBrand} href='/'>
-          <img className={styles.brandLogo}  src="/img/btcswift/bitcoin-swift-logo-main.webp"/>
-          <img className={styles.textLogo}  src="/img/btcswift/bitcoin-swift-text-logo.webp"/>
+          <img className={styles.textLogo}  src="/img/pepenode/logo.svg"/>
+          {/* <img className={styles.textLogo}  src="/img/btcswift/bitcoin-swift-text-logo.webp"/> */}
         </a>
 
         <div className={styles.appnavMenu}>
-          <a href={isStakingPage ? `/${currentLanguage}/#tokenomics` : "#tokenomics"}><img className={styles.menuIcon} src='/img/btcswift/tokenomics.svg'/> {sectionText?.tokenomics}</a>
-          <a href={isStakingPage ? `/${currentLanguage}/#roadmap` : "#roadmap"}><img className={styles.menuIcon} src='/img/btcswift/roadmap.svg'/>  { sectionText?.roadmap}</a>
-          <a href={isStakingPage ? `/${currentLanguage}/#faqs` : "#faqs"} ><img className={styles.menuIcon} src='/img/btcswift/faq.svg'/>  {sectionText?.faq}</a>
-          <a href={isStakingPage ? `/${currentLanguage}/#about` : "#about"}><img className={styles.menuIcon} src='/img/btcswift/whitepaper.svg'/>  {sectionText?.whitePaper}</a>          {/* <a href={`/${currentLanguage}`}>{sectionText?.home}</a> */}
+          <a href={isStakingPage ? `/${currentLanguage}/#tokenomics` : "#tokenomics"}>{sectionText?.home}</a>
+          <a href={isStakingPage ? `/${currentLanguage}/#tokenomics` : "#tokenomics"}>{sectionText?.about}</a>
+          <a href={isStakingPage ? `/${currentLanguage}/#roadmap` : "#roadmap"}>{ sectionText?.howtobuy}</a>
+          <a href={isStakingPage ? `/${currentLanguage}/#roadmap` : "#roadmap"}>{ sectionText?.roadmap}</a>
+          <a href={isStakingPage ? `/${currentLanguage}/#tokenomics` : "#tokenomics"}>{sectionText?.tokenomics}</a>
+          <a href={isStakingPage ? `/${currentLanguage}/#faqs` : "#faqs"} > {sectionText?.faq}</a>
           {/* <a href={isStakingPage ? `/${currentLanguage}/#howtobuy` : "#howtobuy"}><img className={styles.menuIcon} src='/img/btcswift/security.svg'/>  {sectionText?.howtobuy}</a> */}
         </div>
 
@@ -106,6 +108,7 @@ export const Navbar = ({ isStakingPage = false }) => {
           <a href="/"  target='_blank' ><img src='/img/btcswift/twitter2.svg' className={styles.appSocialIcon} /></a>
           <a href='/'  target='_blank' ><img src='/img/btcswift/cmc-1.svg' className={styles.appSocialIcon}/></a>
 
+          <a  className={styles.appWhitepaper} >White Paper</a>
           <button onClick={scrollToBuySection} className={styles.appnavLogin} >{!!currAccount?.address ? truncateMiddle(currAccount?.address) : sectionText?.buyNow}</button>
 
           <div className={styles.langDropdown} >
