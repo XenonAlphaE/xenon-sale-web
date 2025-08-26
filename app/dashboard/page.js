7a2186eaed7d0c2de7ee23d94629b28ce7c04d15
Steve <stevehuytrannd92@gmail.com>
2025-08-26 23:02:44 +0700

    remove dashboard

--- app/dashboard/page.js
+++
@@ -1,6 +0,0 @@
-import StakingApp from '../client-components/stakingapp/StakingApp'
-   
-export default async function Page({ params }) {
-    const lang = (await params).lang
-    return <StakingApp/>
-}
