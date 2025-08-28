a14e8b644110b8acf0527fa4993864aec3bc8da4
Steve <stevehuytrannd92@gmail.com>
2025-08-28 08:51:07 +0700

    change cano

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -35,7 +35,7 @@ export async function generateMetadata({ params }) {
       image: getOG(), // Path to your Open Graph image,
     },
     alternates: {
-      canonical: "https://btchyper.io/dashboard", // Set the canonical URL
+      canonical: "https://btchypercoin.com/dashboard", // Set the canonical URL
     }
 
   };
