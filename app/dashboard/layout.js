da5d27e7527fffe7156e469a6d921a2324cee50f
Steve <stevehuytrannd92@gmail.com>
2025-09-01 12:50:19 +0700

    update cano

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -35,7 +35,7 @@ export async function generateMetadata({ params }) {
       image: getOG(), // Path to your Open Graph image,
     },
     alternates: {
-      canonical: "https://btchype.io/dashboard", // Set the canonical URL
+      canonical: "https://bitcoinhype.io/dashboard", // Set the canonical URL
     }
 
   };
