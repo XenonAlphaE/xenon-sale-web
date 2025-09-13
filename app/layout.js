25fa767537be72dd29d5d710393417ba6e525bb7
Steve <stevehuytrannd92@gmail.com>
2025-09-13 11:22:52 +0700

    update cano

--- app/layout.js
+++ app/layout.js
@@ -43,7 +43,7 @@ export async function generateMetadata() {
       image: getOG(), // Path to your Open Graph image,
     },
     alternates: {
-      canonical: "https://pepenodecoin.com", // Set the canonical URL
+      canonical: "https://pepenode.nl", // Set the canonical URL
     },
   };
 }
